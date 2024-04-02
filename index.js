24470ab94428aba75e1c0dcc5a1bae7eff5a8d27
Gouri Tripathi <gouritripathi@Gouris-MacBook-Air.local>
2024-04-02 15:58:11 +0530

    adding new apis for put,patch,delete

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const appServer = express();
 const PORT = process.env.PORT || 4000;
 
 // This is a list of books that we want to return once the end point is hit.
-const data = [
+const BOOKS = [
 {
     id:1,
     title:"Adventures of Feluda-Vol1",
@@ -36,6 +36,9 @@ const data = [
 }
 ]
 
+appServer.use(express.urlencoded({extended:false}))
+appServer.use(express.json())
+
 appServer.listen(PORT,()=>{
     console.log(`Started listening on server ${PORT}`)
 })
@@ -59,6 +62,50 @@ appServer.get('/contact',(req,res)=>{
 })
 
 appServer.get('/books/favourite',(req,res)=>{
-    res.send(data)
+    res.send(BOOKS)
 })
 
+appServer.put('/update/:id',(req,res)=>{
+    let {id} = req.params
+    console.log('Inside the put command',id,typeof id)
+    
+    id = parseInt(id)
+    if(id){
+        BOOKS.forEach(book=>{
+            if(book.id === id){
+                book.title = book.title+`#${id}`
+                book.author = book.author+`#${id}`
+                console.log(book)
+            }
+        })
+    }
+    console.log(BOOKS)
+    res.send(BOOKS)
+})
+
+appServer.patch('/update/:id',(req,res)=>{
+    let{id} = req.params
+    id = parseInt(id)
+    const data = req.body
+
+    BOOKS.forEach(book=>{
+        if(book.id === id){
+            for (const key in data) {
+               book[key] = data[key]
+            }
+        }
+    })
+    res.send(BOOKS)
+})
+
+appServer.delete('/delete/:id',(req,res)=>{
+        let {id} = req.params
+        id = parseInt(id)
+        const index = BOOKS.findIndex(book=>book.id === id)
+        console.log('delete index',index)
+        if(index>=0){
+            BOOKS.splice(index,1)
+        }
+        res.send(BOOKS)
+        
+})
